8a5b460e2e727728762ba1a2c55150afb62cab3e
Helen Tael <helentael@protonmail.com>
2021-03-09 11:40:57 -0600

    merged with main on github

---
+++ public/src/Footer.js
@@ -0,0 +1,20 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+exports["default"] = void 0;
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+var Footer = function Footer() {
+  return /*#__PURE__*/_react["default"].createElement("div", {
+    id: "footer"
+  }, /*#__PURE__*/_react["default"].createElement("footer", null, "A front-end focused, react-based, retail-client project developed by: Tahsin Ahmed | Daniel Ko | Helen Tael | Zach Thomas"));
+};
+
+var _default = Footer;
+exports["default"] = _default;
+//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uL2NsaWVudC9zcmMvRm9vdGVyLmpzeCJdLCJuYW1lcyI6WyJGb290ZXIiXSwibWFwcGluZ3MiOiI7Ozs7Ozs7QUFBQTs7OztBQUVBLElBQU1BLE1BQU0sR0FBRyxTQUFUQSxNQUFTO0FBQUEsc0JBQ2I7QUFBSyxJQUFBLEVBQUUsRUFBQztBQUFSLGtCQUNFLDRLQURGLENBRGE7QUFBQSxDQUFmOztlQVFlQSxNIiwic291cmNlc0NvbnRlbnQiOlsiaW1wb3J0IFJlYWN0IGZyb20gJ3JlYWN0JztcblxuY29uc3QgRm9vdGVyID0gKCkgPT4gKFxuICA8ZGl2IGlkPVwiZm9vdGVyXCI+XG4gICAgPGZvb3Rlcj5cbiAgICAgIEEgZnJvbnQtZW5kIGZvY3VzZWQsIHJlYWN0LWJhc2VkLCByZXRhaWwtY2xpZW50IHByb2plY3QgZGV2ZWxvcGVkIGJ5OiBUYWhzaW4gQWhtZWQgfCBEYW5pZWwgS28gfCBIZWxlbiBUYWVsIHwgWmFjaCBUaG9tYXNcbiAgICA8L2Zvb3Rlcj5cbiAgPC9kaXY+XG4pXG5cbmV4cG9ydCBkZWZhdWx0IEZvb3RlcjsiXX0=
