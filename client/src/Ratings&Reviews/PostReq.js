0020bda1d142fd5c77a787206d18ad46bae3b8f0
Tahsin Ahmed <tahsinahmed95@gmail.com>
2021-03-09 09:57:57 -0500

    Added new functional rating components and new get requests

--- client/src/Ratings&Reviews/PostReq.js
+++ client/src/Ratings&Reviews/PostReq.js
@@ -1,22 +1,22 @@
 import axios from 'axios';
 import token from '../../../public/token.js';
 
-let data = {
-	product_id: 21111,
-	rating: 5,
-	summary: 'Great Product',
-	body: 'This product was great!',
-	recommend: true,
-	name: 'Tahsin',
-	email: 'email.com',
-	photos: [],
-	characteristics: {
-		70907: 4,
-		70905: 3,
-		70906: 2,
-		70908: 1,
-	},
-};
+// let data = {
+// 	product_id: 21111,
+// 	rating: 5,
+// 	summary: 'Great Product',
+// 	body: 'This product was great!',
+// 	recommend: true,
+// 	name: 'Tahsin',
+// 	email: 'email.com',
+// 	photos: [],
+// 	characteristics: {
+// 		70907: 4,
+// 		70905: 3,
+// 		70906: 2,
+// 		70908: 1,
+// 	},
+// };
 
 export const submitReview = () => {
 	axios({
