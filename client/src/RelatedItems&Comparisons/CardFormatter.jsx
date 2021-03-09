00f77a29ed755a994d0970923e6919a48ea73fc3
Daniel Ko <kod3349@gmail.com>
2021-03-09 17:05:27 -0600

    Refactore Cards to include an image tag instead of Card.Img, allowing easier CSS styling. Gave all images a max height and max width, with a set height of 200px and auto width

--- client/src/RelatedItems&Comparisons/CardFormatter.jsx
+++ client/src/RelatedItems&Comparisons/CardFormatter.jsx
@@ -11,8 +11,8 @@ const CardFormatter = ({id, image, placeHolderImg, category, name, price, rating
     <div>
       <Card border="dark" style={{ width: '16rem', height: '22rem'}}>
         <AddToYourFit />
-        <Card.Img variant="top" src={image[0].thumbnail_url ? image[0].thumbnail_url : placeHolderImg} height="auto" width="100%" onClick={handleClick} id={id}/>
-        <div>
+        {/* <Card.Img variant="top" src={image[0].thumbnail_url ? image[0].thumbnail_url : placeHolderImg} height="200px" width="auto" onClick={handleClick} id={id}/> */}
+          <img class="carousel-img" src={image[0].thumbnail_url ? image[0].thumbnail_url : placeHolderImg} onClick={handleClick} id={id}></img>
           <Card.Body className="text-center" onClick={handleClick} id={id}>
             <Card.Subtitle className="mb-2 text-muted" onClick={handleClick} id={id}>
               {category}
@@ -27,7 +27,6 @@ const CardFormatter = ({id, image, placeHolderImg, category, name, price, rating
               {rating}
             </Card.Text>
           </Card.Body>
-        </div>
       </Card>
     </div>
   )
