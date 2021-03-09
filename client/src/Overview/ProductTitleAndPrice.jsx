71e65ff53bfb5ea84ea2dc222b269097a968bb22
Helen Tael <helentael@protonmail.com>
2021-03-09 11:40:26 -0600

    added stars

--- client/src/Overview/ProductTitleAndPrice.jsx
+++ client/src/Overview/ProductTitleAndPrice.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import StarRatings from 'react-star-ratings';
 
 //recieved props ratings (object), productInfo (object), starRating (number), price (string), salePrice (string)
 var ProductTitleAndPrice = (props) => {
@@ -9,15 +10,18 @@ var ProductTitleAndPrice = (props) => {
 
   return (
     <div className="ProductTitleAndPrice">
-      <div hidden={numRatings === 0}>Star Rating: {props.starRating} <a href='#RatingsAndReviews'>Read all {numRatings} reviews</a></div>
+      <div hidden={numRatings === 0}>Star Rating:
+        <StarRatings rating={props.starRating} starRatedColor="gold" numberOfStars={5} name="rating" />
+        <a href="#RatingsAndReviews">Read all {numRatings} reviews</a>
+      </div>
       <p>{props.productInfo.category}</p>
       <p><b>{props.productInfo.name}</b></p>
       {props.salePrice === null ?
         <p>${props.price}</p>
         :
         <p>
-          <span style={{color: 'red'}}>${props.salePrice} </span>
-          <span style={{textDecoration:'line-through'}}>${props.price}</span>
+          <span style={{color: "red"}}>${props.salePrice} </span>
+          <span style={{textDecoration:"line-through"}}>${props.price}</span>
         </p>
       }
     </div>
