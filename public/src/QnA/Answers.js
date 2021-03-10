9201ab9ff30e931b9e328f94d308469171f183da
Helen Tael <75406341+htael@users.noreply.github.com>
2021-03-10 10:03:45 -0600

    Delete public/src directory

--- public/src/QnA/Answers.js
+++
@@ -1,9 +0,0 @@
-// import React from "react";
-// const Answers = (props) => {
-//   return (
-//     <div>{props.answer}</div>
-//   )
-// }
-// export default Answers
-"use strict";
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbIi4uLy4uLy4uL2NsaWVudC9zcmMvUW5BL0Fuc3dlcnMuanN4Il0sIm5hbWVzIjpbXSwibWFwcGluZ3MiOiJBQUFBO0FBR0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUdBIiwic291cmNlc0NvbnRlbnQiOlsiLy8gaW1wb3J0IFJlYWN0IGZyb20gXCJyZWFjdFwiO1xuXG5cbi8vIGNvbnN0IEFuc3dlcnMgPSAocHJvcHMpID0+IHtcbi8vICAgcmV0dXJuIChcbi8vICAgICA8ZGl2Pntwcm9wcy5hbnN3ZXJ9PC9kaXY+XG4vLyAgIClcbi8vIH1cblxuXG4vLyBleHBvcnQgZGVmYXVsdCBBbnN3ZXJzIl19
