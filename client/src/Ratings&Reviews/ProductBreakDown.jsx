d2355126f2e95422e6cd81ad5e8d0e2c8c0b6df3
Tahsin Ahmed <tahsinahmed95@gmail.com>
2021-03-12 15:15:49 -0500

    Added new modal form and more styling

--- client/src/Ratings&Reviews/ProductBreakDown.jsx
+++ client/src/Ratings&Reviews/ProductBreakDown.jsx
@@ -4,67 +4,62 @@ import Slider from '@material-ui/core/Slider';
 import { createMuiTheme } from '@material-ui/core/styles';
 import { ThemeProvider } from '@material-ui/styles';
 
+
 const muiTheme = createMuiTheme({
-    overrides:{
-      MuiSlider: {
-        thumb:{
-        color: "deeppink",
-        },
-        track: {
-          color: 'deeppink'
-        },
-        rail: {
-          color: 'white'
-        },
-        markLabel: {
-          color:'white',
-          fontWeight:700,
-          transform:'translateX(-100%)',
-        }
-      }
-  }
-  });
+	overrides: {
+		MuiSlider: {
+			thumb: {
+				color: 'deeppink',
+			},
+			track: {
+				color: 'deeppink',
+			},
+			rail: {
+				color: 'white',
+			},
+			markLabel: {
+				color: 'white',
+				fontWeight: 700,
+				transform: 'translateX(-100%)',
+			},
+		},
+	},
+});
 
 const marks = [
-    {
-      value: 3.3,
-    },
-    {
-      value: 6.6,
-    }
-  ];
-
+	{
+		value: 3.3,
+	},
+	{
+		value: 6.6,
+	},
+];
 
 export default function ProductBreakDown({ metaData }) {
-  let metaDataCharacteristics = metaData.characteristics;
-  let characteristics = Object.keys(metaData.characteristics);
+	let metaDataCharacteristics = metaData.characteristics;
+	let characteristics = Object.keys(metaData.characteristics);
 
-  return (
-    characteristics.map((character) => {
-      return (
-        <ThemeProvider theme={muiTheme}>
-          
-          <div className="characteristics">
-            <Typography id="disabled-slider" gutterBottom>
-                {character}
-            </Typography >
-            <Slider
-                disabled defaultValue={metaDataCharacteristics[character].value}
-                aria-labelledby="disabled-slider"
-                max={10}
-                marks={marks}
-            />
-            <div className="sizeSlider">
-                <span>Too Small</span>
-                <span>Perfect</span>
-                <span>Too Large</span>
-            </div>
-          </div>
-        </ThemeProvider>
-      )
-    })
-  );
+	return characteristics.map((character) => {
+		return (
+			<ThemeProvider theme={muiTheme}>
+				<div className="characteristics">
+					<Typography id="disabled-slider" gutterBottom>
+						{character}
+					</Typography>
+					<Slider
+						disabled
+						defaultValue={metaDataCharacteristics[character].value}
+						aria-labelledby="disabled-slider"
+						max={10}
+						marks={marks}
+					/>
+					<div className="sizeSlider">
+						<span>Too Small</span>
+						<span>Perfect</span>
+						<span>Too Large</span>
+					</div>
+				</div>
+			</ThemeProvider>
+		);
+	});
 }
-
-
-
